e12c4868bfabd4e4ef20926b97406cf860af542b
shahin2525 <shahin451999@gmail.com>
2024-08-01 14:04:13 -0700

    add eslint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 const app: Application = express();
-const port = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  const a = 4;
+  res.send(a);
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -14,3 +14,4 @@ async function main() {
     console.log(error);
   }
 }
+main();
