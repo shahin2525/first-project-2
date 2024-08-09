ee2d1f8f58cb334023698651528d88c44dd5a816
shahin2525 <shahin451999@gmail.com>
2024-08-08 20:58:35 -0700

    add custom validatro

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -3,9 +3,21 @@ import { Student } from './student.interface'
 
 // Define the UserName schema
 const userNameSchema = new Schema({
-  firstName: { type: String, required: true },
-  middleName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+    trim: true,
+    maxlength: [20, 'Must be max length 20, character'],
+    validate: {
+      validator: function (value) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1)
+        return firstNameStr === value
+      },
+      message: '{VALUE} is not in capitalize format',
+    },
+  },
+  middleName: { type: String, trim: true },
+  lastName: { type: String, required: [true, 'last name is required'] },
 })
 
 // Define the Guardian schema
@@ -27,21 +39,34 @@ const localGuardianSchema = new Schema({
 
 // Define the Student schema
 const studentSchema = new Schema<Student>({
-  id: { type: String },
+  id: {
+    type: String,
+  },
 
   name: userNameSchema,
-  email: { type: String, required: true },
-  gender: ['male', 'female'],
+  email: { type: String, required: true, unique: true },
+  gender: {
+    type: String,
+    enum: ['male', 'female'],
+    required: true,
+  },
   dateOfBirth: { type: String },
   contactNumber: { type: String, required: true },
   emergencyContact: { type: String, required: true },
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  bloodGroup: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  },
   guardian: guardianSchema,
   localGuardian: localGuardianSchema,
   profileImg: { type: String, required: true },
-  isActive: ['active', 'inActive'],
+  isActive: {
+    type: String,
+    enum: ['active', 'inActive'],
+    default: 'active',
+  },
 })
 
 export const StudentModel = model<Student>('Student', studentSchema)
