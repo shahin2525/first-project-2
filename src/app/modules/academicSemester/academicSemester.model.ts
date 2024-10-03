bd2c997438c9e9a9cae48887a72b3a040ff9f4f1
shahin2525 <shahin451999@gmail.com>
2024-10-02 22:04:03 -0700

    add academic const

---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose'
+import { TAcademicSemester } from './academicSemester.interface'
+import {
+  AcademicSemesterCode,
+  AcademicSemesterName,
+  Months,
+} from './academicSemester.constant'
+
+const academicSemesterSchema = new Schema<TAcademicSemester>({
+  name: {
+    type: String,
+    enum: AcademicSemesterName,
+    required: true,
+  },
+  code: {
+    type: String,
+    enum: AcademicSemesterCode,
+    required: true,
+  },
+  year: {
+    type: Date,
+    required: true,
+  },
+  startMonth: {
+    type: String,
+    enum: Months,
+    required: true,
+  },
+  endMonth: {
+    type: String,
+    enum: Months,
+    required: true,
+  },
+})
+
+const AcademicSemester = model('AcademicSemester', academicSemesterSchema)
+
+export default AcademicSemester
