4939b380ed7129b835aac9c53614243126cca112
shahin2525 <shahin451999@gmail.com>
2024-10-10 22:59:50 -0700

    add transtion

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import config from '../../config'
 import AcademicSemester from '../academicSemester/academicSemester.model'
 import { TStudent } from '../student/student.interface'
@@ -5,6 +6,8 @@ import { Student } from '../student/student.model'
 import { TUser } from './user.interface'
 import User from './user.model'
 import { generateStudentId } from './user.utils'
+import AppError from '../../errors/appError'
+import httpStatus from 'http-status'
 
 const createUserIntoDB = async (password: string, payload: TStudent) => {
   const userData: Partial<TUser> = {}
@@ -17,15 +20,35 @@ const createUserIntoDB = async (password: string, payload: TStudent) => {
   if (!admissionSemester) {
     throw new Error('Admission semester not found')
   }
-  userData.id = await generateStudentId(admissionSemester)
+  const session = await mongoose.startSession()
+  try {
+    session.startTransaction()
+    userData.id = await generateStudentId(admissionSemester)
 
-  const newUser = await User.create(userData)
-  if (Object.keys(newUser).length) {
-    payload.id = newUser.id
-    payload.user = newUser._id
+    // transaction-1
 
-    const newStudent = await Student.create(payload)
-    return newStudent
+    const newUser = await User.create([userData], { session })
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user')
+    }
+    if (newUser.length) {
+      payload.id = newUser[0].id
+      payload.user = newUser[0]._id
+
+      // transaction-2
+      const newStudent = await Student.create([payload], { session })
+
+      if (!newStudent.length) {
+        throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student')
+      }
+
+      await session.commitTransaction()
+      await session.endSession()
+      return newStudent
+    }
+  } catch (err) {
+    await session.abortTransaction()
+    await session.endSession()
   }
 }
 export const UserServices = {
