60825383989b8da4e7776e2b7c79fce72559c5f5
shahin2525 <shahin451999@gmail.com>
2024-10-22 13:23:34 -0700

    add faculty update

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
 import { AdminControllers } from './admin.controller'
+import validRequest from '../../middleware/validateRequest'
+import { AdminValidations } from './admin.validation'
 
 const router = Router()
 router.get('/', AdminControllers.getAllAdmin)
 router.get('/:id', AdminControllers.getSingleAdmin)
-router.patch('/:id', AdminControllers.updateAdmin)
+router.patch(
+  '/:id',
+  validRequest(AdminValidations.updateAdminValidationSchema),
+  AdminControllers.updateAdmin,
+)
 router.delete('/:id', AdminControllers.deleteAdmin)
 export const AdminRoutes = router
--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import { FacultyControllers } from './faculty.controller'
+import validRequest from '../../middleware/validateRequest'
+import { FacultyValidations } from './faculty.validation'
 
 const router = Router()
 router.get('/', FacultyControllers.getAllFaculty)
 router.get('/:id', FacultyControllers.getSingleFaculty)
-router.patch('/:id', FacultyControllers.updateFaculty)
+router.patch(
+  '/:id',
+  validRequest(FacultyValidations.updateFacultyValidationSchema),
+  FacultyControllers.updateFaculty,
+)
 router.delete('/:id', FacultyControllers.deleteFaculty)
 
 export const FacultyRoutes = router
